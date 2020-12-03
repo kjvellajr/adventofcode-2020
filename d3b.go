dc1a8d3090eabff71327c2581fccd667f6b046c2
Ken Vella <kjvellajr@gmail.com>
2020-12-03 13:39:18 -0500

    adding day 3b

---
+++ d3b.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fmt.Println("---day 3b---")
+	data, err := ioutil.ReadFile("input/d3.input")
+	check(err)
+	lines := strings.Split(string(data), "\n")
+	xslope := [...]int{1, 3, 5, 7, 1}
+	yslope := [...]int{1, 1, 1, 1, 2}
+	//xslope := [...]int{3}
+	//yslope := [...]int{1}
+	var result = 1
+	for i, x := range xslope {
+		result *= trees(lines, x, yslope[i])
+	}
+	fmt.Println(result)
+}
+
+func trees(lines []string, right int, down int) int {
+	var x = 0
+	var count = 0
+	for i, line := range lines {
+		if len(line) > 0 && i%down == 0 {
+			if line[x*right%len(line)] == '#' {
+				count++
+			}
+			x++
+		}
+	}
+	fmt.Printf("%d, %d = %d\n", right, down, count)
+	return count
+}
